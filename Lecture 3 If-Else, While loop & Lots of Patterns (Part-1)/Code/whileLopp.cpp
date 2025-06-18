1344acaf844cf04e612af3f32b7359465d3d0d18
pushit chaudhary <pushitcoc@gmail.com>
2025-06-18 20:50:24 +0545

    while loop

---
+++ Lecture 3 If-Else, While loop & Lots of Patterns (Part-1)/Code/whileLopp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int num;
+    cout << "Enter a number :";
+    cin >> num;
+
+    int i = 1;
+
+    while(i <= num){
+        cout << i << "" << endl;
+        i++;
+    }
+
+
+
+}
