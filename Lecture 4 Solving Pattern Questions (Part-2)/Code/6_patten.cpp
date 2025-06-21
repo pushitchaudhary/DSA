850154eb24d26cdb95d3a4c942f0d61e1057ca60
pushit chaudhary <pushitcoc@gmail.com>
2025-06-21 14:26:10 +0545

    patten 6

---
+++ Lecture 4 Solving Pattern Questions (Part-2)/Code/6_patten.cpp
@@ -0,0 +1,36 @@
+/*
+
+
+                *
+
+                *   *
+
+                *   *   *
+
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int num = 4;
+
+    int i = 1;
+
+    while(i<=num){
+        int j = 1;
+        while(j<=i){
+            cout << "*" << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+
+}
