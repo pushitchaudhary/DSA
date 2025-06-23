2597110cd245c18c41b107e6c46143ef0e7a1400
pushit chaudhary <pushitcoc@gmail.com>
2025-06-23 09:37:41 +0545

    patten 12

--- Lecture 4 Solving Pattern Questions (Part-2)/Code/12_patten.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/Code/12_patten.cpp
@@ -10,3 +10,37 @@ A   B   C
 A   B   C   D
 
 */
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int num;
+    cout << "enter any number: ";
+    cin >> num;
+
+    cout << endl;
+
+    int row = 1;
+
+    while(row <= num){
+
+        int col = 1;
+
+        while (col <= row)
+        {
+            char ch = 'A'+ col -1;
+            cout << ch;
+            col++;
+        }
+        
+
+        cout << endl;
+        row++;
+
+    }
+
+
+}
